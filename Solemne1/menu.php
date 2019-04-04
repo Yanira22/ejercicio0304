d6218d1f7df9739dba8e1524b0ed5eeff9c6ac39
cetecom <cetecom@LCT50420>
2019-04-03 22:42:17 -0300

    ya lo que llevo

---
+++ Solemne1/menu.php
@@ -0,0 +1,7 @@
+<div class="list-group">
+    <a class="list-group-item" href="#"><i class="fa fa-file-o fa-2x"></i> HOME</a>
+    <a class="list-group-item" href="#"><i class="fa fa-file-o fa-2x"></i> ABOUT CHILE</a>
+    <a class="list-group-item" href="#"><i class="fa fa-file-o fa-2x"></i> RESERVATION  </a>
+    <a class="list-group-item" href="#"><i class="fa fa-file-o fa-2x"></i> LIST RESERVATION</a>
+</div>
+
